1a697cdaa919da583dfbc2ff6e8bc22220c2bea4
IsisTilmont <isis.tilmont@vdabcampus.be>
2024-02-08 15:02:49 +0100

    Theorie: Eerst testen maken, daarna implementatie

---
+++ src/main/java/Rekening.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class Rekening {
+    private BigDecimal saldo = BigDecimal.ZERO;
+
+    void stort (BigDecimal bedrag){
+        saldo = saldo.add(bedrag);
+    }
+    BigDecimal getSaldo(){
+        return saldo;
+    }
+}

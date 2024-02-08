f748561859e45e05ce4fab17dffd100b67296594
IsisTilmont <isis.tilmont@vdabcampus.be>
2024-02-08 15:30:44 +0100

    Taken: Test fixtures

--- taken/src/test/java/VeilingTest.java
+++ taken/src/test/java/VeilingTest.java
@@ -1,25 +1,32 @@
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 public class VeilingTest {
+
+    private Veiling veiling;
+
+    @BeforeEach
+    void beforeEach(){
+        veiling = new Veiling();
+    }
     @Test
     void hoogsteBodVanNieuweVeilingIs0 () {
-        assertThat(new Veiling().getHoogsteBod()).isZero();
+        assertThat(veiling.getHoogsteBod()).isZero();
     }
 
     @Test
     void nadatJeBodVan10UitvoertIsHoogsteBod10(){
-        var veiling = new Veiling();
         veiling.doeBod(BigDecimal.TEN);
         assertThat(veiling.getHoogsteBod()).isEqualByComparingTo("10");
     }
 
     @Test
     void nadatJeBodVan10en2UitvoertIsHoogsteBod20() {
-        var veiling = new Veiling();
         veiling.doeBod(BigDecimal.TEN);
         veiling.doeBod(BigDecimal.TWO);
         assertThat(veiling.getHoogsteBod()).isEqualByComparingTo("10");

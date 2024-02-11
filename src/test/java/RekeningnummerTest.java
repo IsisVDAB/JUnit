d0141919159bedb3510b6cd546a4734c65b19f9a
IsisTilmont <isis.tilmont@vdabcampus.be>
2024-02-11 12:37:58 +0100

    Theorie + Taak: Parameterized test

--- src/test/java/RekeningnummerTest.java
+++ src/test/java/RekeningnummerTest.java
@@ -3,6 +3,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class RekeningnummerTest {
     @Test void BE72091012240116IsCorrect() {
         // De constructor oproepen met een correct rekeningnummer mag geen exception werpen.
@@ -10,13 +13,33 @@ class RekeningnummerTest {
         // Je moet daarvoor geen assertThat typen.
         new Rekeningnummer("BE72091012240116");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings= { "BE72091012240116", "BE68539007547034",
+            "BE02063588295840"})
+    void correteNummers(String nummer){
+        new Rekeningnummer(nummer);
+    }
+
+    // Vervangen door ParameterizedTest:
+    /*
     @Test void BE68539007547034IsCorrect() {
         new Rekeningnummer("BE68539007547034");
     }
     @Test void BE02063588295840IsCorrect() {
         new Rekeningnummer("BE02063588295840"); // met eencontrolegetal < 10
+    }*/
+
+    @ParameterizedTest
+    @ValueSource(strings= { "BE720910122401160", "BE7209101224011",
+            "NL72091012240116", "BEX209101224011", "BE01091012240116",
+            "BE99091012240116", "BE72091012240115", ""})
+    void verkeerdeNummers(String nummer){
+        assertThatIllegalArgumentException().isThrownBy(() -> new Rekeningnummer(nummer));
     }
-    @Test void BE720910122401160IsTeLang() {
+
+    // Vervangen door parametrized test:
+/*    @Test void BE720910122401160IsTeLang() {
         assertThatIllegalArgumentException().isThrownBy(() ->
                 new Rekeningnummer("BE720910122401160"));
     }
@@ -44,13 +67,15 @@ class RekeningnummerTest {
         assertThatIllegalArgumentException().isThrownBy(() ->
                 new Rekeningnummer("BE72091012240115"));
     }
-    @Test void nummerMetNullIsVerkeerd() {
-        assertThatNullPointerException().isThrownBy(() -> new Rekeningnummer(null));
-    }
+
     @Test void leegNummerIsVerkeerd() {
         assertThatIllegalArgumentException().isThrownBy(() ->
                 new Rekeningnummer(""));
+    }*/
+    @Test void nummerMetNullIsVerkeerd() {
+        assertThatNullPointerException().isThrownBy(() -> new Rekeningnummer(null));
     }
+
     @Test void toStringGeeftHetNummerTerug() {
         var nummer = "BE72091012240116";
         assertThat(new Rekeningnummer(nummer)).hasToString(nummer);
--- taken/src/test/java/IsbnTest.java
+++ taken/src/test/java/IsbnTest.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 public class IsbnTest {
-
+/*
     @Test
     void nummer0isVerkeerd() {
         assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(0));
@@ -39,6 +42,22 @@ public class IsbnTest {
     @Test
     void eenNummerMet13CijfersMetCorrectControleGetal0() {
         new Isbn(9789227439640L);
+    }
+    @Test
+    void toStringMoetHetNummerTeruggeven() {
+        assertThat(new Isbn(9789227439640L)).hasToString("9789227439640");
+    }*/
+
+    @ParameterizedTest
+    @ValueSource(longs = { 9789027439642L, 9789227439640L })
+    void correcteNummers(long nummer) {
+        new Isbn(nummer);
+    }
+    @ParameterizedTest
+    @ValueSource(longs = { 0, -9789027439642L, 978902743964L, 97890274396421L,
+            9779227439643L, 8789027439642L, 9789027439643L })
+    void verkeerdeNummers(long nummer) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Isbn(nummer));
     }
     @Test
     void toStringMoetHetNummerTeruggeven() {

ee1c1363b173d4654a75db862223fd0662727a20
IsisTilmont <isis.tilmont@vdabcampus.be>
2024-02-11 10:05:21 +0100

    StatistiekTest

--- src/test/java/StatistiekTest.java
+++ src/test/java/StatistiekTest.java
@@ -11,19 +11,23 @@ public class StatistiekTest {
         assertThat(Statistiek.getGemiddelde(new BigDecimal[]{BigDecimal.ZERO, BigDecimal.TEN
         })).isEqualByComparingTo("5");
     }
+
     @Test void hetGemiddeldeVanEenGetalIsDatGetal() {
         var enigGetal = BigDecimal.valueOf(1.23);
         assertThat(Statistiek.getGemiddelde(new BigDecimal[] { enigGetal }))
                 .isEqualByComparingTo(enigGetal);
     }
+
     @Test void hetGemiddeldeVanEenLegeVerzamelingKanJeNietBerekenen() {
         assertThatIllegalArgumentException().isThrownBy(
                 () -> Statistiek.getGemiddelde(new BigDecimal[] {}));
     }
+
     @Test void hetGemiddeldeVanNullKanJeNietBerekenen() {
         assertThatNullPointerException().isThrownBy(
                 () -> Statistiek.getGemiddelde(null));
     }
+
     @Test void eenArrayElementMagNietNullBevatten() {
         assertThatNullPointerException().isThrownBy(
                 () -> Statistiek.getGemiddelde(new BigDecimal[] { null }));

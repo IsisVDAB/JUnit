570e7c4c0e2c5f76b8e4d38c662236d28d81a66c
IsisTilmont <isis.tilmont@vdabcampus.be>
2024-02-12 17:31:43 +0100

    Taak: Mock

--- taken/src/test/java/WoonplaatsServiceTest.java
+++ taken/src/test/java/WoonplaatsServiceTest.java
@@ -1,16 +1,31 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
-public class WoonplaatsServiceTest {
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class) public class WoonplaatsServiceTest {
     private WoonplaatsService service;
+    @Mock
+    private WoonplaatsRepository repository;
+
     @BeforeEach
     void beforeEach(){
-        service = new WoonplaatsService(new WoonplaatsRepositoryStub());
+        service = new WoonplaatsService(repository);
     }
 
     @Test
     void eenWoonplaatsBevatMaximaal3Streepjes(){
+        when(repository.findMetStreepjes()).thenReturn(
+                List.of("Sint-Niklaas", "Sint-Ama-nds-berg", "Sint-Katelijne"));
         assertThat(service.maxAantalStreepjesInEenWoonplaats()).isEqualTo(3L);
+        verify(repository).findMetStreepjes();
     }
 }

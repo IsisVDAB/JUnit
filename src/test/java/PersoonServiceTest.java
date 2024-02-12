178d3ce8b5d0c243f9bb95739aa19a1ab789bf17
IsisTilmont <isis.tilmont@vdabcampus.be>
2024-02-12 17:22:58 +0100

    Theorie: Mock

--- src/test/java/PersoonServiceTest.java
+++ src/test/java/PersoonServiceTest.java
@@ -1,18 +1,33 @@
 import net.bytebuddy.implementation.bytecode.Throw;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
-public class PersoonServiceTest {
-    private PersoonService service;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class) public class PersoonServiceTest {
+    private PersoonService service;
+    @Mock
+    private PersoonRepository repository;
     @BeforeEach
     void beforeEach() {
-        service = new PersoonService(new PersoonRepositoryStub());
+        service = new PersoonService(repository);
     }
 
     @Test
     void deWeddeStandaardAfwijkingIs2(){
+        when(repository.findAllWeddes()).thenReturn(
+                Stream.of(2, 4, 4, 4, 5, 5, 7, 9)
+                        .map(getal -> BigDecimal.valueOf(getal))
+                                .toList());
         assertThat(service.standaardAfwijkingWeddes()).isEqualByComparingTo("2");
+        verify(repository).findAllWeddes();
     }
 }
